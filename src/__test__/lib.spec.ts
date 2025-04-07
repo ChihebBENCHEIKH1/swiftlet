bc5429e9fb9fbc3fba0e073cc61ec2219db8f763
chiheb ben cheikh <bencheikh.chiheb@gmail.com>
2025-04-07 22:44:35 +0100

    minor fixes

--- src/__test__/lib.spec.ts
+++ src/__test__/lib.spec.ts
@@ -5,7 +5,7 @@ const server: Swiftlet = new Swiftlet(1939, undefined, true);
 server.route({
   endpoint: "/",
   method: "get",
-  callback: () => console.log("ping pong!"),
+  callback: (req: any) => console.log("ping pong!", req.query("test")),
   res: {
     statusCode: 201,
     json: {
@@ -17,7 +17,7 @@ server.route({
 server.route({
   endpoint: "/test",
   method: "get",
-  callback: (req: any) => console.log("ping pong!", req.query("b")),
+  callback: () => console.log("hello world!"),
   res: {
     statusCode: 201,
     json: {
